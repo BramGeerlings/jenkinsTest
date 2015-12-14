4f44a6392a73f8572d6587c3d8ffc212bb070cc3
Joosten, RKJ (Rosanne) <rosanne.joosten@rabobank.nl>
2015-12-14 17:04:09 +0100

    Verneuken

--- src/main/java/Persoon.java
+++ src/main/java/Persoon.java
@@ -2,26 +2,26 @@
  * Created by GeerlingsB and RosanneJ on 11-12-2015.
  */
 public class Persoon {
-    private String Naam;
-    private String Achternaam;
+    private String naam;
+    private String achternaam
 
     public Persoon(String naam) {
-        Naam = naam;
+        this.naam = naam;
     }
 
     public String getNaam() {
-        return Naam;
+        return naam;
     }
 
     public void setNaam(String naam) {
-        Naam = naam;
+        this.naam = naam;
     }
 
     public String getAchternaam() {
-        return Achternaam;
+        return achternaam;
     }
 
     public void setAchternaam(String achternaam) {
-        Achternaam = achternaam;
+        this.achternaam = achternaam
     }
 }

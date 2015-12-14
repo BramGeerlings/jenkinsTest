76adbb4873e247a3fb7e79b40670ea247b37688d
Joosten, RKJ (Rosanne) <rosanne.joosten@rabobank.nl>
2015-12-14 12:53:20 +0100

    Class renamed so "Naam" can be used within the class

--- src/main/java/Naam.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by GeerlingsB on 11-12-2015.
- */
-public class Naam {
-}
---
+++ src/main/java/Persoon.java
@@ -0,0 +1,18 @@
+/**
+ * Created by GeerlingsB and RosanneJ on 11-12-2015.
+ */
+public class Persoon {
+    private String Naam;
+
+    public Persoon(String naam) {
+        Naam = naam;
+    }
+
+    public String getNaam() {
+        return Naam;
+    }
+
+    public void setNaam(String naam) {
+        Naam = naam;
+    }
+}

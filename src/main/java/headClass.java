49f10e02c4874c906fc998d5f2a831ba011605a8
Joosten, RKJ (Rosanne) <rosanne.joosten@rabobank.nl>
2015-12-16 10:07:40 +0100

    .git geplakt aan Jenkins repository path en SCM poll aangezet

--- src/main/java/headClass.java
+++ src/main/java/headClass.java
@@ -2,7 +2,7 @@
  * Created by JoostenRKJ on 15-12-2015.
  */
 public class headClass {
-// push voor .git in repository path Jenkins change
+// push voor .git in repository path Jenkins change..
     public static void main (String[] args){
         String onthouden = "http://kwxbuild.rabobank.nl:8080/jenkins/adjuncts/fe99a441/org/kohsuke/stapler/jquery/jquery.full.js";
         Persoon Bram = new Persoon("Geerlings", "Bram");
